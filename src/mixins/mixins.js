7e003d032690b9e245392db68e5b02e08265a625
Adebola Adeola <debola_adeola@yahoo.com>
2021-04-29 14:01:36 +0100

    minor

--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -1,38 +1,5 @@
 export default {
   methods: {
-    checkStock(product) {
-
-      let available = (product) => { return true }
-      let notAvailable = () => { return false }
-      let checkTotalStock = (product) => { // no variant
-        product.total_stock > product.count ? available() : notAvailable() }
-      let checkStockCount = (product) => { // one variant
-        product.this_stock > product.count ? available() : notAvailable() }
-      let checkComboQty = (product) => { // two variants
-        product.combo_qty > product.count ? available() : notAvailable() }
-      let howManyVariants = (product) => {
-        product.multiple_variants ? checkComboQty() : checkStockCount() }
-
-      product.has_variant ? howManyVariants(product) : checkTotalStock(product)
-
-
-
-      // if(product.combo_stock) {
-      //   if (product.combo_stock > product.count) {
-      //     return true
-      //   }
-      // } else if(product.this_stock) {
-      //   if (product.this_stock > product.count) {
-      //     return true
-      //   }
-      // } else {
-      //   if(product.total_stock > product.count) {
-      //     return true
-      //   }
-      // }
-      // alert("Not enough in stock (from inside cart) to increase")
-      // return false
-    },
     numberOnly(event) {
       const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
       const keyPressed = event.key
